caeed0295e43ec755b3aacb7f5085d504fb09eeb
Marcao <marcos.jesus@cliqx.com.br>
2023-09-01 23:48:47 -0300

    add: method setValue

--- src/app/add-folk/add-folk.component.ts
+++ src/app/add-folk/add-folk.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
-
 @Component({
   selector: 'app-add-folk',
   templateUrl: './add-folk.component.html',
   styleUrls: ['./add-folk.component.css']
 })
+
 export class AddFolkComponent {
   name = new FormControl('');
 
